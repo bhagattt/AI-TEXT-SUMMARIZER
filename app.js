f69affcae370d016c8497fee470be178164eba52
bhagattt <aryanbhagat850@gmail.com>
2024-07-13 18:32:18 +0530

    Final

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json()); 
+
+const HUGGING_FACE_API_KEY = "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"; 
+
+async function summarizeText(req, res) {
+    const { text } = req.body;
+    if (!text) {
+        return res.status(400).send('Text is required');
+    }
+
+    try {
+
+        const response = await axios.post(
+            'https://api-inference.huggingface.co/models/facebook/bart-large-cnn',
+            { inputs: text },
+            {
+                headers: {
+                    'Authorization': `Bearer ${HUGGING_FACE_API_KEY}`, 
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
+
+        
+        const summary = response.data[0].summary_text;
+        res.json({ summary });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while summarizing the text');
+    }
+}
+
+app.post('/summarize', summarizeText);
+
+app.listen(3000);
